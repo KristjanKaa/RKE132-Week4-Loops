f37570214305be0762f562c4cfc1e48699bf5d3e
KristjanKaa <159649951+KristjanKaa@users.noreply.github.com>
2024-02-23 13:59:49 +0200

    Added my project

---
+++ WhileLoops/Program.cs
@@ -0,0 +1,26 @@
+﻿
+Random rnd = new Random();
+int cpurandom;
+
+bool loopActive = true; //bool -true/false
+
+while(loopActive)
+{
+    cpurandom = rnd.Next(1, 4);
+    //Console.WriteLine($"Mcpu has generated {cpurandom}.");
+
+    Console.WriteLine("Make a guess. Enter a number 1-3");
+    int useNumber = Int32.Parse(Console.ReadLine());
+
+    if(useNumber == cpurandom)
+
+    { Console.WriteLine("Congratulations, You won!");
+        //loopActive = false;
+        break;
+    }
+    else
+    { 
+            Console.WriteLine("Sorry. Try again!");
+        }
+    }
+Console.WriteLine("Have a nice day!");
